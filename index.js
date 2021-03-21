000c91245e3f134df2f924dc27771f1c20e26a8b
Tajir Hasnain <u1604081@student.cuet.ac.bd>
2021-03-21 15:34:47 +0600

    intial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import {
+  GetDivisions,
+  GetDistrictsFiltered,
+  GetDistrictsUnfiltered,
+  IsADivision,
+} from "./methods";
+
+export default {
+  GetDivisions,
+  GetDistrictsFiltered,
+  GetDistrictsUnfiltered,
+  IsADivision,
+};
